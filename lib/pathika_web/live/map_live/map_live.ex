d39042a1c345330eab89c12a77a2cb99db990ca7
Jack Kellenberger <jackson@kellenberger.io>
2022-11-17 10:09:02 -0500

    Adds on hover highlighting, contents toggling

--- lib/pathika_web/live/map_live/map_live.ex
+++ lib/pathika_web/live/map_live/map_live.ex
@@ -10,14 +10,23 @@ defmodule PathikaWeb.MapLive do
 
   def render(assigns) do
     ~H"""
+    <style>
+      .hex:hover {
+        fill: gray;
+        transition: all ease 0.3s;
+      }
+      .hex: {
+        transition: all ease 0.3s;
+      }
+    </style>
     <div class="map-container">
       <svg width="1000" height="1000" viewBox="0 0 1000 1000">
-        <g class="hex_map">
+        <g id="hex_map">
         <%= for {coord, hex} <- @hex_grid do %>
-          <polygon id={coord} phx-click={"select-" <> coord} points={hex.points} stroke="gray" fill="black"/>
+          <polygon class="hex" id={coord} phx-click={"select-" <> coord} points={hex.points} stroke="gray" fill="black"/>
           <%= if hex.contents do %>
-          <circle cx={hex.center_x} cy={hex.center_y} fill="white" r="8"/>
-          <text text-anchor="middle" x={hex.center_x} y={hex.center_y - 20} fill="white" > <%= coord%> </text>
+          <circle phx-click={"select-" <> coord} cx={hex.center_x} cy={hex.center_y} fill="white" r="8"/>
+          <text phx-click={"select-" <> coord} text-anchor="middle" x={hex.center_x} y={hex.center_y - 20} fill="white" > <%= coord%> </text>
           <% end %>
         <% end %>
         </g>
@@ -28,7 +37,9 @@ defmodule PathikaWeb.MapLive do
 
   def handle_event("select-" <> hex_number, _, socket) do
     map = socket.assigns.hex_grid
-    hex = Map.get(map, hex_number) |> Map.put(:contents, true)
+    hex = Map.get(map, hex_number)
+    hex = Map.put(hex, :contents, !hex.contents)
+
     hex_grid = %{map | hex_number => hex}
     {:noreply, assign(socket, :hex_grid, hex_grid)}
   end
