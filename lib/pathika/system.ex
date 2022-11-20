83163d932f28986beca8feed9ab458bf2bd7650c
Jack Kellenberger <jackson@kellenberger.io>
2022-11-20 11:35:45 -0500

    Adds data entry box for text parser to map view

--- lib/pathika/system.ex
+++ lib/pathika/system.ex
@@ -47,7 +47,7 @@ defmodule Pathika.System do
     :stellar
   ]
 
-  @t5_header_to_keys %{
+  @t5_fields_to_keys %{
     "hex" => :hex,
     "name" => :name,
     "uwp" => :uwp,
@@ -63,23 +63,35 @@ defmodule Pathika.System do
     "a" => :allegiance,
     "stellar" => :stellar
   }
-  @header_t5 "Hex  Name                 UWP       Remarks                                  {Ix}   (Ex)    [Cx]   N     B  Z PBG W  A    Stellar"
-  @separator_t5 "---- -------------------- --------- ---------------------------------------- ------ ------- ------ ----- -- - --- -- ---- --------------"
 
   def sample do
     """
-    Name                                   Hex  UWP       Remarks                                  {Ix}   (Ex)    [Cx]   N     B  Z PBG W  A    Stellar
-    -------------------------------------- ---- --------- ---------------------------------------- ------ ------- ------ ----- -- - --- -- ---- --------------
-    Zeycude                                0101 C430698-9 De Na Ni Po                              { -1 } (C53-1) [6559] -     -  - 613 8  ZhIN K9 V
-    Reno                                   0102 C4207B9-A De He Na Po Pi Pz                        { 1 }  (C6A+2) [886B] -     -  A 603 12 ZhIN G8 V M1 V
-    Errere                                 0103 B563664-B Ni Ri O:0304                             { 3 }  (957+1) [4939] -     KM - 910 9  ZhIN M1 V M4 V
-    Cantrel                                0104 C566243-9 Lo                                       { -1 } (610-4) [1126] -     -  - 520 12 ZhIN F1 V
-    Gyomar                                 0108 C8B2889-8 Fl He Ph (Tethmari)                      { -1 } (G77+1) [9769] -     -  - 824 14 NaHu A8 V
-    Thengo                                 0202 C868586-5 Ag Ni Pr                                 { -1 } (743-2) [4444] -     -  - 801 12 ZhIN G5 V M3 V
-    Rio                                    0301 C686648-8 Ag Ni Ga Ri                              { 0 }  (954+1) [6658] -     -  - 201 8  NaHu M0 V M1 V
-    GesentownGesentownGesentown            0303 B31169B-C Ic Na Ni Da                              { 2 }  (956+4) [887E] -     KM A 801 12 ZhIN M2 V M9 V
-    Chronor                                0304 A6369A5-D Hi Cp                                    { 4 }  (C8G+2) [7D3B] -     KM - 810 5  ZhIN F8 V
-    Atsa                                   0307 B4337CA-A Na Po An Pz
+    Hex  Name                 UWP       Remarks                                  {Ix}   (Ex)    [Cx]   N     B  Z PBG W  A    Stellar
+    ---- -------------------- --------- ---------------------------------------- ------ ------- ------ ----- -- - --- -- ---- --------------
+    0101 Zeycude              C430698-9 De Na Ni Po                              { -1 } (C53-1) [6559] -     -  - 613 8  ZhIN K9 V
+    0102 Reno                 C4207B9-A De He Na Po Pi Pz                        { 1 }  (C6A+2) [886B] -     -  A 603 12 ZhIN G8 V M1 V
+    0103 Errere               B563664-B Ni Ri O:0304                             { 3 }  (957+1) [4939] -     KM - 910 9  ZhIN M1 V M4 V
+    0104 Cantrel              C566243-9 Lo                                       { -1 } (610-4) [1126] -     -  - 520 12 ZhIN F1 V
+    0108 Gyomar               C8B2889-8 Fl He Ph (Tethmari)                      { -1 } (G77+1) [9769] -     -  - 824 14 NaHu A8 V
+    0202 Thengo               C868586-5 Ag Ni Pr                                 { -1 } (743-2) [4444] -     -  - 801 12 ZhIN G5 V M3 V
+    0301 Rio                  C686648-8 Ag Ni Ga Ri                              { 0 }  (954+1) [6658] -     -  - 201 8  NaHu M0 V M1 V
+    0303 Gesentown            B31169B-C Ic Na Ni Da                              { 2 }  (956+4) [887E] -     KM A 801 12 ZhIN M2 V M9 V
+    0304 Chronor              A6369A5-D Hi Cp                                    { 4 }  (C8G+2) [7D3B] -     KM - 810 5  ZhIN F8 V
+    0307 Atsa                 B4337CA-A Na Po An Pz                              { 3 }  (A6C+5) [9A7C] -     KM A 810 8  ZhIN F7 V M3 V
+    0503 Whenge               D648500-8 Ag Ni                                    { -2 } (842-5) [1313] -     -  - 610 13 NaHu F8 V
+    0601 Enlas-du             E975776-6 Ag Pi                                    { -1 } (966-2) [6645] -     -  - 323 14 NaHu F1 V
+    0605 Algebaster           C665658-9 Ag Ni Ga Ri                              { 1 }  (955+1) [6759] -     -  - 410 10 NaHu M0 V M1 V
+    0607 Rasatt               E883401-7 Ni                                       { -3 } (631-5) [1113] -     -  - 910 6  NaHu F0 V
+    0608 Ninjar               A311666-C Ic Na Ni Mr                              { 2 }  (956+1) [584B] -     KM - 410 6  ZhIN A4 V
+    0610 Sheyou               B756779-A Ag Ga                                    { 4 }  (B6D+5) [8B6B] -     KM - 111 13 ZhIN F4 V M0 V
+    0703 Indo                 E434662-6 Ni O:0605                                { -3 } (851-5) [2312] -     -  - 320 11 NaHu F6 V
+    0704 Nerewhon             E738475-7 Ni                                       { -3 } (631-5) [2135] -     -  - 820 10 NaHu K5 V
+    0705 Cipango              A886865-C Ga Ri Pa Ph O:0304                       { 4 }  (D7E+2) [6C3A] -     KM - 121 10 ZhIN G2 V
+    0710 Stave                E7667A8-2 Ag Ga Ri (Obeyery)                       { 0 }  (965+1) [7752] -     -  - 801 8  NaHu K9 V M2 V
+    0805 Narval               D525688-7 Ni Da                                    { -3 } (851-3) [6357] -     M  A 603 10 CsZh G4 V M6 V
+    0807 Plaven               E845300-5 Lo                                       { -3 } (520-5) [1111] -     -  - 910 6  NaHu G8 V M7 V
+    0808 Quar                 B532720-B Na Po Pz                                 { 2 }  (A6C-2) [2916] -     N  A 401 9  CsIm M2 V
+    0810 Frond                E9C3300-9 Fl Lo                                    { -2 } (820-5) [1114] -     -  - 103 12 CsIm F8 V
     """
   end
 
@@ -87,9 +99,10 @@ defmodule Pathika.System do
     {key, data} =
       data
       |> String.split("\n", trim: true)
+      |> Enum.map(&String.trim/1)
       |> parse_headers()
 
-    data |> Enum.map(&build_system(&1, key))
+    Enum.map(data, &build_system(&1, key))
   end
 
   defp parse_headers([header, separator | data]) do
@@ -109,14 +122,13 @@ defmodule Pathika.System do
     field_positions(separators, postions, max + 2)
   end
 
-  @spec build_system(any, map) :: any
-  def build_system(data, fields) do
+  defp build_system(data, fields) do
     Map.keys(fields)
     |> Enum.reduce(%__MODULE__{}, fn key, system -> put_attribute(key, system, data, fields) end)
   end
 
   defp put_attribute(key, system, data, fields) do
     value = String.slice(data, fields[key]) |> String.trim()
-    Map.put(system, @t5_header_to_keys[key], value)
+    Map.put(system, @t5_fields_to_keys[key], value)
   end
 end
--- lib/pathika_web/live/map_live/map_live.ex
+++ lib/pathika_web/live/map_live/map_live.ex
@@ -1,21 +1,16 @@
 defmodule PathikaWeb.MapLive do
   use PathikaWeb, :live_view
   alias Pathika.HexGrid
-  alias Pathika.Names
+  alias Pathika.System
 
   def mount(_, _, socket) do
     grid = HexGrid.new(8, 10)
-
-    subsector =
-      HexGrid.coordinates(8, 10)
-      |> Enum.map(&HexGrid.coordinate_to_string/1)
-      |> Map.new(&{&1, nil})
 
     socket =
       assign(socket,
         hex_grid: grid,
         page_title: "SectorMaker",
-        subsector: subsector
+        systems: System.parse_data(System.sample())
       )
 
     {:ok, socket}
@@ -40,52 +35,82 @@ defmodule PathikaWeb.MapLive do
         border-width: 3px;
         background-color: black;
       }
+      textarea {
+        overflow-x: scroll;
+        overflow-y: scroll;
+        resize: none;
+      }
+
     </style>
-    <div id="map-container">
-      <svg id="hex-map" height="800px" viewBox="0 0 500 728">
-        <g phx-click="select">
-        <%= for {coord, hex} <- @hex_grid do %>
-            <polygon class="hex" id={"hex-" <> coord} points={hex} stroke="gray" fill="black"/>
-        <% end %>
-        <%= for {hex, system} <- Enum.filter(@subsector, fn {_k, v} -> v end) do %>
-          <% {x, y} = HexGrid.center_point(hex) %>
-          <circle class="click-through" cx={x} cy={y} fill="white" r="8"/>
-          <text class="click-through" text-anchor="middle" x={x} y={y - 23} fill="white" font-family="Optima,  sans-serif", font-size="x-small" > <%= hex %> </text>
-          <text class="click-through" text-anchor="middle" x={x} y={y + 25} fill="white" font-family="Optima,  sans-serif", font-size="smaller" > <%= system %> </text>
-        <% end %>
-        </g>
-      </svg>
+    <div id="wrapper">
+
+      <div id="map-container">
+        <svg id="hex-map" height="800px" viewBox="0 0 500 728">
+          <g phx-click="select">
+          <%= for {coord, hex} <- @hex_grid do %>
+              <polygon class="hex" id={"hex-" <> coord} points={hex} stroke="gray" fill="black"/>
+          <% end %>
+          <%= for system <- @systems do %>
+            <% {x, y} = HexGrid.center_point(system.hex) %>
+            <%= if system.travel_zone == "A" do %>
+            <circle class="click-through" cx={x} cy={y} stroke="orange" fill="none" r="30"/>
+            <% end%>
+            <%= if system.travel_zone == "R" do %>
+            <circle class="click-through" cx={x} cy={y} stroke="red" fill="none" r="30"/>
+            <% end%>
+            <circle class="click-through" cx={x} cy={y} fill="white" r="5"/>
+            <text class="click-through" text-anchor="middle" x={x} y={y - 23} fill="white" font-family="Optima,  sans-serif", font-size="x-small" > <%= system.hex %> </text>
+            <text class="click-through" text-anchor="middle" x={x} y={y - 10} fill="white" font-family="Optima,  sans-serif", font-size="x-small" > <%= String.slice(system.uwp, 0..0) %> </text>
+            <text class="click-through" text-anchor="middle" x={x} y={y + 25} fill="white" font-family="Optima,  sans-serif", font-size="smaller" > <%= system.name %> </text>
+            <%= if system.bases == "N" do %>
+              <text class="click-through" text-anchor="middle" x={x - 20} y={y} fill="white" font-family="Optima,  sans-serif", font-size="x-small" > <%= system.bases %> </text>
+            <% end%>
+          <% end %>
+          </g>
+        </svg>
+      </div>
+      <div id="data-input">
+        <.form let={f} for={:sector} phx-change="update">
+          <%= textarea f , :data, wrap: "off", cols: "80", rows: "10", spellcheck: "false", phx_debounce: "500", value: System.sample()%>
+        </.form>
+      </div>
     </div>
     """
   end
 
-  def handle_event(
-        "select",
-        %{"target" => "hex-" <> hex_number, "altKey" => false},
-        socket
-      ) do
-    subsector =
-      Map.update(socket.assigns.subsector, hex_number, nil, fn system ->
-        case system do
-          nil -> Names.random_world_name()
-          _ -> system
-        end
-      end)
+  # def handle_event(
+  #       "select",
+  #       %{"target" => "hex-" <> hex_number, "altKey" => false},
+  #       socket
+  #     ) do
+  #   subsector =
+  #     Map.update(socket.assigns.subsector, hex_number, nil, fn system ->
+  #       case system do
+  #         nil -> Names.random_world_name()
+  #         _ -> system
+  #       end
+  #     end)
 
-    socket = assign(socket, subsector: subsector)
+  #   socket = assign(socket, subsector: subsector)
 
+  #   {:noreply, socket}
+  # end
+
+  def handle_event("update", %{"sector" => data}, socket) do
+    systems = data["data"] |> System.parse_data()
+    socket = assign(socket, :systems, systems)
     {:noreply, socket}
   end
 
-  def handle_event("select", %{"target" => "hex-" <> hex_number, "altKey" => true}, socket) do
-    subsector = Map.put(socket.assigns.subsector, hex_number, nil)
+  # def handle_event("select", %{"target" => "hex-" <> hex_number, "altKey" => true}, socket) do
+  #   subsector = Map.put(socket.assigns.subsector, hex_number, nil)
 
-    socket = assign(socket, subsector: subsector)
+  #   socket = assign(socket, subsector: subsector)
 
-    {:noreply, socket}
-  end
+  #   {:noreply, socket}
+  # end
 
-  def handle_event("select", %{"target" => _}, socket) do
+  def handle_event("select", _, socket) do
     {:noreply, socket}
   end
 end
